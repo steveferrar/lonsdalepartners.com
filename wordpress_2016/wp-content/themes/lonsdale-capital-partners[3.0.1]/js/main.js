00ae8d100ed0cd5ab40dea08b521ce9f282858ec
Steve Ferrar <steveferrar@gmail.com>
2016-05-02 12:56:50 +0100

    Reduce button size, bring header up, amend mobile hover for home buttons

--- wordpress_2016/wp-content/themes/lonsdale-capital-partners[3.0.1]/js/main.js
+++ wordpress_2016/wp-content/themes/lonsdale-capital-partners[3.0.1]/js/main.js
@@ -44,96 +44,42 @@ $(document).ready(function(){
 	// ---------------------------------------------
 
 
-	// Mobile Menu
 	$('#nav-icon').click(function(){
 		$(this).toggleClass('open');
 		$('body').toggleClass('js-stopScrolling');
 		$('.js-mobile-menu').stop().slideToggle('fast');
 	});
 
-	 // Projects Menu
-	$('.page-projects .js-projects-menu-button').click(function(e){
-		e.preventDefault();
-	});
-
-	$('.post-type-archive .js-projects-menu-button').click(function(e){
-		e.preventDefault();
-		$(this).toggleClass('active');
-		$('.js-projects-menu').stop().slideToggle('fast');
-	});
-
 
 	// ----- Google Maps
 	// ---------------------------------------------
-	/*
-	if($('body').hasClass('page-contact')){
 
-		function initialize() {
-			
-			// var myLatlng = new google.maps.LatLng(51.523001,-0.08069350000005215); // 23 Curtain Road
-			var myLatlng = new google.maps.LatLng(googleLat,googleLng);
-			
-			var mapOptions = {
-				zoom: 15,
-				center: myLatlng,
-				scrollwheel: false,
-				draggable: false,
-				zoomControl: true,
-				disableDoubleClickZoom: false,
-				mapTypeId: google.maps.MapTypeId.ROADMAP
+
+	if($('html').hasClass('touch')){
+		// Mobile
+		//console.log('Mobile');
+		$('a.js-home-block').click(function(e){
+			if($(this).hasClass('active')) {
+			} else {
+				e.preventDefault();
+				$('a.js-home-block').removeClass('active');
+				$(this).toggleClass('active');
 			}
-					
-			var map = new google.maps.Map(document.getElementById('google_map'), mapOptions);
-
-			var stylez = [
-				{
-				  featureType: "all",
-				  elementType: "all",
-				  stylers: [
-					{ saturation: -100 } // <-- THIS
-				  ]
-				}
-			];
-			
-			var mapType = new google.maps.StyledMapType(stylez, { name:"Grayscale" });    
-			map.mapTypes.set('tehgrayz', mapType);
-			map.setMapTypeId('tehgrayz');
-			
-			var interval = setTimeout(function () {
-				var marker = new google.maps.Marker({
-					draggable: false,
-					animation: google.maps.Animation.DROP,
-					raiseOnDrag: false,
-					position: myLatlng, 
-					map: map,
-					title: 'Tourist',
-					icon: new google.maps.MarkerImage(
-						templateUrl + '/img/map-pin@2x.png', // my 16x48 sprite with 3 circular icons
-						null,
-						null,
-						null,
-						new google.maps.Size(31, 40) // scaled size of the entire sprite
-					)
-				});
-			}, 1500);
-
-			var center;
-			function calculateCenter() {
-			  center = map.getCenter();
+		});
+	} else {
+		// Non Mobile
+		//console.log('Not Mobile');
+		$('a.js-home-block').hover(
+			function() {
+				// console.log('in');
+				$(this).addClass('active');
+			},
+			function() {
+				// console.log('out');
+				$(this).removeClass('active');
 			}
-
-			google.maps.event.addDomListener(map, 'idle', function() {
-			  calculateCenter();
-			});
-
-			google.maps.event.addDomListener(window, 'resize', function() {
-				map.setCenter(center);
-			});
-		}
-			
-		google.maps.event.addDomListener(window, 'load', initialize);
-	
+		);
 	}
-	*/
+
 	
 });
