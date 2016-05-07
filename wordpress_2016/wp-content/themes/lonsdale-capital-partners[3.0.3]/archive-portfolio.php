f3c28539312131a555ac9ce4b8cce68de19f34cc
Steve Ferrar <steveferrar@gmail.com>
2016-05-07 23:25:47 +0100

    Update 3.0.3
    Remove flexible content plugin, add search query filter

---
+++ wordpress_2016/wp-content/themes/lonsdale-capital-partners[3.0.3]/archive-portfolio.php
@@ -0,0 +1,110 @@
+<?php
+/**
+	The template for displaying the PORTFOLIO archive.
+*/
+get_header();
+if ( have_posts() ) :
+?>  
+		
+	<section class="archive archive__portfolio"> 
+		
+		<div class="container">
+
+			<div class="row porfolio__row">
+
+				<div class="archive__portfolio-content">
+
+
+					<!-- Filter -->
+					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+						<div class="block block--tall block--padding portfolio__filter">
+
+							<h2>Filter Our Portfolio by Sector</h2>	
+												
+							<?php echo do_shortcode( '[ULWPQSF id=451 button=0]' ); ?>
+
+							<p class="portfolio__filter-clear js-filter-clear"><a href="<?php echo get_post_type_archive_link('portfolio'); ?>">Clear All</a></p>
+
+						</div>
+					</div>
+
+					
+					<!-- Portfolio Items -->
+					<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
+
+						<div class="row js-filter">
+
+							<?php
+							$i = 1;	
+							while ( have_posts() ) :
+							the_post();
+							if(get_field('thumbnail')):
+								$portfolio_image = wp_get_attachment_image_src(get_field('thumbnail'), 'block-image' );
+							else:
+								$portfolio_image = wp_get_attachment_image_src(get_field('image'), 'block-image' );
+							endif;
+
+							// Set Background Colour
+						 	if(get_field('background_colour')): 
+						 		$background_colour = ' style="background-color:' . get_field('background_colour') . ' ;"';
+					 		else:
+					 			$background_colour = '';
+							endif;
+							?>
+
+								<div class="col-xxs-12 col-xs-6<?php if(0 == ($i % 2)) { ?> pull-xs-right<?php }; ?> col-sm-6<?php if(0 == ($i % 2)) { ?> pull-sm-right<?php }; ?> hidden-md hidden-lg portfolio__image">
+									<a class="block block--image" href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>">
+										<img src="<?php echo $portfolio_image[0]; ?>" alt="<?php the_title(); ?>" data-no-retina />
+									</a>
+								</div>
+
+								<div class="col-xxs-12 col-xs-6 col-sm-6 col-md-4 col-lg-4 portfolio__info">
+									<a class="block block--padding" href="<?php echo get_permalink(); ?>" title="<?php the_title(); ?>"<?php echo $background_colour; ?>>
+
+					                    <h2><?php the_title(); ?></h2>
+					                    <div class="portfolio__single-information">
+					                        <?php if (get_field('investment_date')): echo '<p><strong>Investment date:</strong> ' . get_field('investment_date') . '</p>'; endif; ?>
+					                        <?php if (get_field('exit_date')): echo '<p><strong>Exit date:</strong> ' . get_field('exit_date') . '</p>'; endif; ?>
+					                        <?php if (get_field('sector')): echo '<p><strong>Sector:</strong> ' . get_field('sector') . '</p>'; endif; ?>
+						                    <?php if (get_field('company_website')): ?> 
+												<p class="website">
+													<strong>Company website:</strong> <?php echo get_field('company_website'); ?>
+													<?php if (get_field('company_website_2')): echo '<br />' . get_field('company_website_2'); endif; ?>
+												</p>
+											<?php endif; ?>
+						                    <?php if(get_field('logo')):
+						                        $logo = wp_get_attachment_image_src(get_field('logo'), 'full' ); ?>
+						                        <img src="<?php echo $logo[0]; ?>" title="<?php the_title(); ?>" class="portfolio__single-image" data-no-retina />
+						                    <?php endif; ?>
+					                    </div>
+
+										<p class="portfolio__archive-back">Read More &rsaquo;</p>
+
+									</a>
+								</div>
+
+							<?php
+							$i++;
+							endwhile;
+							?>
+														
+						</div>
+
+					</div>
+
+
+					<div class="clearfix"></div>
+
+
+				</div>
+
+			</div>
+		
+		</div>
+
+	</section>
+	
+<?php
+endif;
+get_footer();
+?>
---
+++ wordpress_2016/wp-content/themes/lonsdale-capital-partners[3.0.3]/js/main.js
@@ -0,0 +1,135 @@
+
+/* $(document).ready
+---------------------------------------- */
+
+$(document).ready(function(){
+	
+	// "use strict";
+
+
+	// console.log('Template URL: ' + templateUrl);
+	// console.log('Google Map Latitude: ' + googleLat);
+	// console.log('Google Map Longitude: ' + googleLng);
+
+
+	// ----- Window Resize
+	// ---------------------------------------------
+
+	$(window).height();
+	$(window).width();
+
+	$(window).resize(function() {
+		$(window).height();
+		$(window).width();
+	});
+
+
+	// ----- No JS
+	// ---------------------------------------------
+
+	$('html').removeClass('no-js');
+
+
+	// ----- Lazyload
+	// ---------------------------------------------
+
+	$("img.lazyload").lazyload({
+		effect : "fadeIn"
+	});
+
+
+	// ----- Mobile Menu
+	// ---------------------------------------------
+
+	// Main Mobile Navigation
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+		$('body').toggleClass('js-stopScrolling');
+		$('.js-mobile-menu').stop().slideToggle('fast');
+	});
+
+	// Portfolio Filters
+	$('.js-portfolio-button').click(function(){
+		$(this).toggleClass('active');
+		$(this).next('.js-portfolio-menu').stop().slideToggle('fast');
+	});
+
+
+	// ----- Google Maps
+	// ---------------------------------------------
+
+	if($('html').hasClass('touch')){
+		// Mobile
+		//console.log('Mobile');
+		$('a.js-home-block').click(function(e){
+			if($(this).hasClass('active')) {
+			} else {
+				e.preventDefault();
+				$('a.js-home-block').removeClass('active');
+				$(this).toggleClass('active');
+			}
+		});
+	} else {
+		// Non Mobile
+		//console.log('Not Mobile');
+		$('a.js-home-block').hover(
+			function() {
+				// console.log('in');
+				$(this).addClass('active');
+			},
+			function() {
+				// console.log('out');
+				$(this).removeClass('active');
+			}
+		);
+	}
+
+
+	// ----- Portfolio Filter
+	// ---------------------------------------------
+
+	// Show / hide filter on <span> click
+	$('.uwpqsf_class span').click(function(){
+		if($(this).parent().hasClass('active')){
+			$('.uwpqsf_class').removeClass('active');
+		} else {
+			$('.uwpqsf_class').removeClass('active');
+			$(this).parent().addClass('active');
+		}
+	});
+
+	// Hide filter on <label> click
+	$('.uwpqsf_class label').click(function(){
+		if($(this).parent().hasClass('active')){
+			$('.uwpqsf_class').removeClass('active');
+		} else {
+			if($(this).hasClass('active')){
+				$(this).removeClass('active');
+			} else {
+				$(this).addClass('active');
+			}
+		}
+		
+		// var thisID = $(this).children('input').attr('id');
+		// console.log(thisID);
+		
+		// Show clear link
+		if($('.uwpqsf_class label').hasClass('active')){
+			$('.js-filter-clear').show();
+		} else {
+			$('.js-filter-clear').hide();
+		}
+	});
+
+	// Hide filter on click anywhere else
+	$(document).click(function(event) { 
+		if(!$(event.target).closest('.uwpqsf_class').length &&
+		!$(event.target).is('.uwpqsf_class')) {
+			if($('.uwpqsf_class').hasClass('active')) {
+				$('.uwpqsf_class').removeClass('active');
+			}
+		}
+	})
+
+
+});
